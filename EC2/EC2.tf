3ea5ceab5616ed4dbbb44a9a08ce16038b65230d
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-12 22:50:56 +0530

    Update EC2.tf

--- EC2/EC2.tf
+++ EC2/EC2.tf
@@ -71,7 +71,7 @@ resource "aws_instance" "ec2-instance" {
     key_name                    = var.key_name
     associate_public_ip_address = true
     security_groups             = [aws_security_group.wp-sg-tf.id]
-    user_data                   = file("installing-components.sh")
+    user_data                   = file("EC2/installing-components.sh")
     tags = {
       Name = "ec2-instance"
     }
