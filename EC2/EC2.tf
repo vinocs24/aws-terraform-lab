b16a1e056ed37d2be588523a1300cab3c29d283d
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-12 16:57:46 +0530

    Update EC2.tf

--- EC2/EC2.tf
+++ EC2/EC2.tf
@@ -13,37 +13,3 @@ resource "aws_instance" "ec2-instance" {
       Name = "ec2-instance"
     }
 }
-
-
-# SG
-
-resource "aws_security_group" "wp-sg-tf" {
-  name        = "wp-instance-tf"
-  description = "Security group for EC2 Instances"
-  vpc_id      = "aws_vpc.default.id"
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "wp-sg-tf"
-  }
-}
