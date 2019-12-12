dfc356df1dd924507a1fe8ae5074a7fc8ca7a559
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-12 16:38:19 +0530

    Create installing-components.sh

---
+++ EC2/installing-components.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo su
+yum update -y
+yum install httpd php php-mysql -y
+cd /var/www/html
+echo "healthy" > healthy.html
+wget https://wordpress.org/wordpress-5.1.1.tar.gz
+tar -xzf wordpress-5.1.1.tar.gz
+mv wordpress/wp-config-sample.php wordpress/wp-config.php
+cp -r wordpress/* /var/www/html/
+rm -rf wordpress
+rm -rf wordpress-5.1.1.tar.gz
+chmod -R 755 wp-content
+chown -R apache:apache wp-content
+chkconfig httpd on
+service httpd start
