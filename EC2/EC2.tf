24c563b64e55378df256f9368d840c36c1cd6b41
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-12 20:02:29 +0530

    Update EC2.tf

--- EC2/EC2.tf
+++ EC2/EC2.tf
@@ -4,10 +4,10 @@ resource "aws_instance" "ec2-instance" {
     ami                         = var.ami
     instance_type               = var.instance_type
     availability_zone           = "us-west-2a"
-    subnet_id                   = "var.subnet_id"
+    subnet_id                   = "var.subnet_id_wa"
     key_name                    = var.key_name
     associate_public_ip_address = true
-    security_groups             = [var.security_groups]
+    security_groups             = [var.security_groups_wa]
     user_data                   = file("EC2/installing-components.sh")
     tags = {
       Name = "ec2-instance"
