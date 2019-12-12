c0ad0db7412d96fdf845838dd868a482d685bb6e
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-12 22:50:22 +0530

    Update Variable.tf

--- EC2/Variable.tf
+++ EC2/Variable.tf
@@ -8,6 +8,21 @@ variable "key_name" {
   default     = "rds-key"
 }
 
+variable "vpc_cidr_block" {
+  description = "VPC network"
+  default     = "10.1.0.0/16"
+}
+
+variable "public_subnet_cidr_block" {
+  description = "Public Subnet"
+  default     = "10.1.1.0/24"
+}
+
+variable "private_subnet_cidr_block" {
+  description = "Private Subnet"
+  default     = "10.1.2.0/24"
+}
+
 variable "availability_zones" {
   description = "Availability Zones"
   default     = "us-west-2a,us-west-2b,us-west-2c"
@@ -23,9 +38,42 @@ variable "instance_type" {
   default = "t2.micro"
 }
 
-variable "vpc_id" {}
-variable "subnet_id_wa" {}
-variable "security_groups_wa" {}
+variable "db_instance_type" {
+  description = "RDS instance type"
+  default = "db.t2.micro"
+}
 
+variable "db_name" {
+  description = "RDS DB name"
+  default = "wordpressdb"
+}
 
+variable "db_user" {
+  description = "RDS DB username"
+  default = "example"
+}
 
+variable "db_password" {
+  description = "RDS DB password"
+  default = "Qwerty12345-"
+}
+
+variable "wp_title" {
+  description = "Wordpress title"
+  default = "My Wordpress on ECS"
+}
+
+variable "wp_user" {
+  description = "Wordpress username"
+  default = "admin"
+}
+
+variable "wp_password" {
+  description = "Wordpress password"
+  default = "Qwerty12345-"
+}
+
+variable "wp_mail" {
+  description = "Wordpress email"
+  default = "vinocs24@gmail.com"
+}
