36d0321b2df03b34c0d94293f9d6dec631e7509d
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-12 17:11:00 +0530

    Update EC2.tf

--- EC2/EC2.tf
+++ EC2/EC2.tf
@@ -1,13 +1,18 @@
 # EC2 Instances
 
+#module "ec2_cluster" {
+ 
+
 resource "aws_instance" "ec2-instance" {
+    source                      = "aws-terraform-lab/VPC/VPC"
+    version                     = "~> 2.0"
     ami                         = var.ami
     instance_type               = var.instance_type
     availability_zone           = "us-west-2a"
-    subnet_id                   = "module.VPC.aws_subnet.wp-public-tf.id"
+    subnet_id                   = aws_subnet.wp-public-tf.id
     key_name                    = var.key_name
     associate_public_ip_address = true
-    security_groups             = ["module.VPC.aws_security_group.wp-sg-tf.id"]
+    security_groups             = [aws_security_group.wp-sg-tf.id]
     user_data                   = file("EC2/installing-components.sh")
     tags = {
       Name = "ec2-instance"
