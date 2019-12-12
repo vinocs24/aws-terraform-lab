bfbcde65880ffe3edd677a202bdf2e19455bba68
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-12 16:50:57 +0530

    Update EC2.tf

--- EC2/EC2.tf
+++ EC2/EC2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "ec2-instance" {
     ami                         = var.ami
     instance_type               = var.instance_type
     availability_zone           = "us-west-2a"
-    subnet_id                   = aws_subnet.wp-public-tf.id
+    subnet_id                   = "module.VPC.aws_subnet.wp-public-tf.id"
     key_name                    = var.key_name
     associate_public_ip_address = true
     security_groups             = [aws_security_group.wp-sg-tf.id]
