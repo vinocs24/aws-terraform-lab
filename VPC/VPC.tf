a09c6f742182176df434e69562bf28c6a1b27fd2
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-12 16:57:49 +0530

    Update VPC.tf

--- VPC/VPC.tf
+++ VPC/VPC.tf
@@ -54,3 +54,36 @@ resource "aws_route_table" "wp-rt-public-tf" {
        Name = "wp-rt-public-tf"
     }
 }
+
+# SG
+
+resource "aws_security_group" "wp-sg-tf" {
+  name        = "wp-instance-tf"
+  description = "Security group for EC2 Instances"
+  vpc_id      = "aws_vpc.default.id"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "wp-sg-tf"
+  }
+}
