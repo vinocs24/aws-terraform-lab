bfeced691ae728be753c64767598fe1d039bae00
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-13 14:14:49 +0530

    Update Var.tf

--- ELB/Var.tf
+++ ELB/Var.tf
@@ -1,1 +1,19 @@
+variable "vpc_cidr_block" {
+  description = "VPC network"
+  default     = "10.1.0.0/16"
+}
 
+variable "public_subnet_cidr_block" {
+  description = "Public Subnet"
+  default     = "10.1.1.0/24"
+}
+
+variable "private_subnet_cidr_block" {
+  description = "Private Subnet"
+  default     = "10.1.2.0/24"
+}
+
+variable "availability_zones" {
+  description = "Availability Zones"
+  default     = "us-west-2a,us-west-2b,us-west-2c"
+}
