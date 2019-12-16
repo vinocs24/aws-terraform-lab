0dd5d7bb48e0e0fdf8c25af630b3e7414d8d26b6
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2019-12-16 22:49:22 +0530

    Update Main.tf

--- RDS/Main.tf
+++ RDS/Main.tf
@@ -1,7 +1,7 @@
 resource "aws_db_subnet_group" "default" {
     name        = "wp-db-subnet-tf"
     description = "VPC Subnets"
-    subnet_ids  = [aws_subnet.wp-public-tf.id, aws_subnet.wp-private-tf.id]
+    subnet_ids  = [var.subnet-pub, var.subnet-pri]
 }
 
 resource "aws_db_instance" "wordpress" {
@@ -15,7 +15,7 @@ resource "aws_db_instance" "wordpress" {
     username               = var.db_user
     password               = var.db_password
     availability_zone      = "us-west-2b"
-    vpc_security_group_ids = [aws_security_group.wp-db-sg-tf.id]
+    vpc_security_group_ids = [security-group-db]
     multi_az               = false
     db_subnet_group_name   = aws_db_subnet_group.default.id
     parameter_group_name   = "default.mysql5.7"
